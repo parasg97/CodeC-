930caf05ef61341fc68d7cc3f7ea7651047995ef
Paras <parasg1997@gmail.com>
2018-12-09 19:23:36 +0530

    dijstra

--- conceptCodes/graph/dijsktra.cpp
+++ conceptCodes/graph/dijsktra.cpp
@@ -1,28 +1,43 @@
 
+
 #include <bits/stdc++.h> 
 #define pb push_back 
   
 using namespace std; 
   
-unordered_set <char> visited;
-unordered_map<char,vector<pair<char,int>> g;//weighted graph
-map<char,int> d;//dijsktra helper
-unordered_map<char,int> res;//result
+unordered_set <int> visited;
+unordered_map< int, vector< pair<int,int> > > g;//node,neighbour/weight
+multimap<int,int> pq;//current dist from root,node
 
-void addEdge(char a,char b,int w){
-	g[a].pb(pair<char,int>(b,w));
+int main(){
+	int n,m;
+	cin>>n>>m;
+	vector<int> res(n+1,INT_MAX);
+	res[1]=0;
 
-	//if its bidirected
-	//g[b].pb(pair<char,int>(a,w));	
-}
-
-void findSpath(char root){
-	res[root]=0;
-	d.insert(root,0);
-	while(!d.empty()){
-		vector<pair<char,int> v=g[d.begin().first];
-		visited.insert(d.begin().first);
-		d.pop();
-
+	for (int i = 0; i < m; ++i)
+	{
+		int u,v,w;
+		cin>>u>>v>>w;
+		g[u].pb(make_pair(v,w));
+	}
+	pq.insert(make_pair(0,1));
+	visited.insert(1);
+	while(!pq.empty()){
+		int n=pq.begin()->second;
+		int w=pq.begin()->first;
+		pq.erase(pq.begin());
+		std::vector< pair<int,int> > v=g[n];
+		for(int i=0;i<v.size();i++){
+			res[v[i].first]=min(res[n]+v[i].second,res[v[i].first]);
+			if(visited.find(v[i].first)==visited.end()){
+				pq.insert(make_pair(res[v[i].first],v[i].first));
+				visited.insert(v[i].first);
+			}
+		}
+	}
+	for(int i=2;i<res.size();i++){
+		cout<<res[i]<<" ";
 	}
 }
+
