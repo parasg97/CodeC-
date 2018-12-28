b2b6fc464ac1a4b14e242429de0a924c413def3e
Paras <parasg1997@gmail.com>
2018-12-28 11:32:03 +0530

    changes

--- conceptCodes/graph/minSpanTree/krusal.cpp
+++ conceptCodes/graph/minSpanTree/krusal.cpp
@@ -1,60 +1,67 @@
-//https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/tutorial/
-
 #include <bits/stdc++.h>
+
+#define pb push_back
 
 using namespace std;
 
-unordered_map<int,int> size;//keep track of number of nodes in subsets
-unordered_map<int,int> g;//node and its parent
-multimap<int,pair<int,int>>m;//edge weight,node a to node b
+
+unordered_map<int,int> uf; // node , parent
+multimap<int,pair<int,int>> pq;//priority queue , weight/u and v
+unordered_map<int,int> size;
+
 
 
 int findParent(int a){
-	while(g[a]!=a){
-		//a=g[a];
-		a=g[g[a]];//path compresion
-	}
-	return a;
+    while(uf[a]!=a){
+        a=uf[uf[a]];
+    }
+    return a;
 }
 
 void doUnion(int a,int b){
-	int p1=findParent(a);
-	int p2=findParent(b);
-	if(size[p1]>size[p2]){
-		g[p2]=p1;
-		size[p1]+=size[p2];
-	}else{
-		g[p1]=p2;
-		size[p2]+=size[p1];
-	}
+    int p1=findParent(a);
+    int p2=findParent(b);
+    if(p1==p2){
+        return;
+    }else{
+        if(size[p1]>size[p2]){
+            uf[p2]=p1;
+            size[p1]+=size[p2];
+        }else{
+            uf[p1]=p2;
+            size[p2]+=size[p1];
+        }
+    }
 }
 
 
-int main(){
-	int n,e;
-	int w,u,v;
-	int ans=0;
-	cin>>n>>e;
-	for(int i=0;i<e;i++){
-		cin>>u>>v>>w;
-		g[u]=u;
-		size[u]=1;
-		g[v]=v;
-		size[v]=1;
-		//m[w]=make_pair(u,v);
-		m.insert(make_pair(w,make_pair(u,v)));
-	}
-	for(auto itr=m.begin();itr!=m.end();itr++){
-	    int n1=itr->second.first;
-	    int n2=itr->second.second;
-	    int p1=findParent(n1);
-	    int p2=findParent(n2);
-	    if(p1!=p2){
-	        ans=ans+itr->first;
-	        doUnion(n1,n2);
-	    }
-	}
-	
-	cout<<ans<<endl;
-	
+int main() {
+    int n,e,w;
+    scanf("%d%d",&n,&e);
+    for(int i=0;i<e;i++){
+        int u,v;
+        scanf("%d%d%d",&u,&v,&w);
+        uf[u]=u;
+        uf[v]=v;
+        size[u]=1;
+        size[v]=1;
+        pq.insert({w,{u,v}});
+    
+    }
+    int ans=0;
+    while(!pq.empty()){
+        auto itr=pq.begin();
+        int u=itr->second.first;
+        int v=itr->second.second;
+        int p1=findParent(u);
+        int p2=findParent(v);
+        if(p1!=p2){
+            ans+=itr->first;
+            doUnion(u,v);
+        }
+        pq.erase(itr);
+    }
+    cout<<ans<<endl;
 }
+
+
